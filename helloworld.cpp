58fb7c608651132a086bfbdc65e161d94a0d3fe7
Peeja Khorashadi <khorashadi.peeja@gmail.com>
2022-07-13 11:08:25 -0600

    hello world cpp

---
+++ helloworld.cpp
@@ -0,0 +1,14 @@
+//hello world C++
+/* this is
+a multi-line comment
+we use the */
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout<< "hello world";
+    return 0;
+}
